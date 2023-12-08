2eb8afb739c34da6dbad3aed696bfd4e4a7d0c17
Wolf Mermelstein <wolf.mermelstein@cwru.edu>
2023-12-07 23:11:09 -0500

    Create test class template

---
+++ src/test/java/org/main/AirportSystemTest.java
@@ -0,0 +1,26 @@
+package org.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AirportSystemTest {
+
+    @org.junit.jupiter.api.Test
+    void addEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void shortestDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void minimumSpanningTree() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void breadthFirstSearch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
